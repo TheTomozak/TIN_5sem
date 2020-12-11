10133321eb946c1f0013751684674cbea92d5de9
TheTomozak <tomaszpalczewski99@gmail.com>
2020-12-11 21:37:50 +0100

    Dodano dla produktu edytowanie, usuwanie i dodawanie do bazy danych przez strone

--- MP2/tin-projekt-palczewski-s18969/controllers/produktyController.js
+++ MP2/tin-projekt-palczewski-s18969/controllers/produktyController.js
@@ -19,7 +19,7 @@ exports.showAddProduktForm = (req, res, next) => {
         pageTitle: 'Nowy produkt',
         formMode: 'createNew',
         btnLabel: 'Dodaj',
-        formAction: '/Produkt/add',
+        formAction: '/produkty/add',
         navLocation: 'produkt'
     });
 }
@@ -33,7 +33,7 @@ exports.showEditProduktForm = (req, res, next) => {
                 formMode: 'edit',
                 pageTitle: 'Edycja produktu',
                 btnLabel: 'Edytuj',
-                formAction: '/Produkt/edit',
+                formAction: '/produkty/edit',
                 navLocation: 'produkt'
             });
         });
@@ -54,3 +54,30 @@ exports.showProduktDetails = (req, res, next) => {
 }
 
 
+
+exports.addProdukt = (req, res, next) => {
+
+    const produkData = { ...req.body };
+    ProduktRepository.createProdukt(produkData)
+        .then(result => {
+            res.redirect('/produkty');
+        });
+
+};
+
+exports.updateProdukt = (req, res, next) => {
+    const idProduktu = req.body.IdProdukt ;
+    const produkData = { ...req.body };
+    ProduktRepository.updateProdukt(idProduktu, produkData)
+        .then(result => {
+            res.redirect('/produkty');
+        });
+};
+
+exports.deleteProdukt = (req, res, next) => {
+    const idProduktu = req.params.idProduktu;
+    ProduktRepository.deleteProdukt(idProduktu)
+        .then(() => {
+            res.redirect('/produkty');
+        });
+};
--- MP2/tin-projekt-palczewski-s18969/routes/produktyRoute.js
+++ MP2/tin-projekt-palczewski-s18969/routes/produktyRoute.js
@@ -8,6 +8,10 @@ router.get('/add', produktyControler.showAddProduktForm);
 router.get('/edit/:idProduktu', produktyControler.showEditProduktForm);
 router.get('/details/:idProduktu', produktyControler.showProduktDetails);
 
+router.post('/add', produktyControler.addProdukt); 
+router.post('/edit', produktyControler.updateProdukt);
+router.get('/delete/:idProduktu', produktyControler.deleteProdukt);
+
 module.exports = router;
 
 
