af53250a31734e03ba463c58a9d0d64f059a541c
TheTomozak <tomaszpalczewski99@gmail.com>
2020-12-14 15:27:12 +0100

    Aktualizacja walidacji po stronie serwera dla pozycji zamowien, dziala w 95%(nie dziala zapamiętywanie poprzedniej wartości)

--- MP2/tin-projekt-palczewski-s18969/controllers/pozycjaZamowienController.js
+++ MP2/tin-projekt-palczewski-s18969/controllers/pozycjaZamowienController.js
@@ -16,65 +16,95 @@ exports.showPozycjaZamowieniaList = (req, res, next) => {
 }
 
 
-exports.showPozycjaZamowieniaForm = (req, res, next) => {
-    let allZamowienia, allpozycjaZamowien, allProdukty;
-    ZamowieniaRepository.getZamowienia()
-        .then(zamowienie => {
-            allZamowienia = zamowienie;
-        
-        })
 
-        ProduktRepository.getProdukt()
-        .then(produkty => {
-            allProdukty = produkty;
-            return PozycjaZamowienRepository.getZamowienieProdukt();
-        })
-        .then(pozycjaZamowien => {
-            allpozycjaZamowien = pozycjaZamowien;
-            res.render('Pages/PozycjaZamówień/FormularzNowejPozycjiZamówień', {
-                pozycjaZamowien: {},
-                formMode: 'createNew',
-                allZamowienia: allZamowienia,
-                allpozycjaZamowien: allpozycjaZamowien,
-                allProdukty: allProdukty,
-                pageTitle: 'Nowa pozycja zamówień',
-                btnLabel: 'Dodaj',
-                formAction: '/pozycjaZamowien/add',
-                navLocation: 'pozycjaZamowien'
-            });
+exports.showPozycjaZamowieniaForm = (req, res, next) => {
+    let zamowienieGetAll = ZamowieniaRepository.getZamowienia();
+    let produktGetAll = ProduktRepository.getProdukt();
+    let pozycjaZamowienGetAll = PozycjaZamowienRepository.getZamowienieProdukt();
+
+
+    Promise.all([zamowienieGetAll, produktGetAll, pozycjaZamowienGetAll]).then(results => {
+        const [zam, prod, pozZam] = results;
+        res.render('Pages/PozycjaZamówień/FormularzNowejPozycjiZamówień', {
+            pozycjaZamowien: {},
+            formMode: 'createNew',
+            allZamowienia: zam,
+            allProdukty: prod,
+            allpozycjaZamowien: pozZam,
+            pageTitle: 'Nowa pozycja zamówień',
+            btnLabel: 'Dodaj',
+            formAction: '/pozycjaZamowien/add',
+            navLocation: 'pozycjaZamowien',
+            validationErrors: []
         });
-}
+
+
+
+    });
+};
+
 
 
 exports.showEditPozycjaZamowieniaForm = (req, res, next) => {
     const idPozycjaZamowien = req.params.idPozycjaZamowien;
-    let allZamowienia, allpozycjaZamowien;
-    ZamowieniaRepository.getZamowienieById(idPozycjaZamowien)
-        .then(zamowienie => {
-            allZamowienia = zamowienie;
-        
-        })
 
-        ProduktRepository.getProduktById(idPozycjaZamowien)
-        .then(produkty => {
-            allProdukty = produkty;
-            return PozycjaZamowienRepository.getZamowienieProduktById(idPozycjaZamowien);
-        })
-        .then(pozycjaZamowien => {
-            allpozycjaZamowien = pozycjaZamowien;
-            res.render('Pages/PozycjaZamówień/FormularzNowejPozycjiZamówień', {
-                pozycjaZamowien: pozycjaZamowien,
-                formMode: 'edit',
-                allZamowienia: allZamowienia,
-                allpozycjaZamowien: allpozycjaZamowien,
-                allProdukty: allProdukty,
-                pageTitle: 'Edycja pozycji zamówień',
-                btnLabel: 'Edytuj',
-                formAction: '/pozycjaZamowien/edit',
-                navLocation: 'pozycjaZamowien'
-            });
+    let zamowienieGetAll = ZamowieniaRepository.getZamowienieById(idPozycjaZamowien);
+    let produktGetAll = ProduktRepository.getProduktById(idPozycjaZamowien);
+    let pozycjaZamowienGetAll = PozycjaZamowienRepository.getZamowienieProduktById(idPozycjaZamowien);
+
+    Promise.all([zamowienieGetAll, produktGetAll, pozycjaZamowienGetAll]).then(results => {
+        const [zam, prod, pozZam] = results;
+        res.render('Pages/PozycjaZamówień/FormularzNowejPozycjiZamówień', {
+            pozycjaZamowien: pozZam,
+            formMode: 'edit',
+            pageTitle: 'Edycja pozycji zamówień',
+            btnLabel: 'Edytuj',
+            formAction: '/pozycjaZamowien/edit',
+            navLocation: 'pozycjaZamowien',
+            validationErrors: [],
+            allZamowienia: zam,
+            allProdukty: prod,
+            allpozycjaZamowien: pozZam
         });
+
+
+
+    });
+
+
 };
+
+// exports.showEditPozycjaZamowieniaForm = (req, res, next) => {
+//     const idPozycjaZamowien = req.params.idPozycjaZamowien;
+//     let allZamowienia, allpozycjaZamowien;
+//     ZamowieniaRepository.getZamowienieById(idPozycjaZamowien)
+//         .then(zamowienie => {
+//             allZamowienia = zamowienie;
+
+//         })
+
+//     ProduktRepository.getProduktById(idPozycjaZamowien)
+//         .then(produkty => {
+//             allProdukty = produkty;
+//             return PozycjaZamowienRepository.getZamowienieProduktById(idPozycjaZamowien);
+//         })
+//         .then(pozycjaZamowien => {
+//             allpozycjaZamowien = pozycjaZamowien;
+//             res.render('Pages/PozycjaZamówień/FormularzNowejPozycjiZamówień', {
+//                 pozycjaZamowien: pozycjaZamowien,
+//                 formMode: 'edit',
+//                 allZamowienia: allZamowienia,
+//                 allpozycjaZamowien: allpozycjaZamowien,
+//                 allProdukty: allProdukty,
+//                 pageTitle: 'Edycja pozycji zamówień',
+//                 btnLabel: 'Edytuj',
+//                 formAction: '/pozycjaZamowien/edit',
+//                 navLocation: 'pozycjaZamowien',
+//                 validationErrors: []
+
+//             });
+//         });
+// };
 
 exports.showPozycjaZamowieniaDetails = (req, res, next) => {
 
@@ -94,7 +124,8 @@ exports.showPozycjaZamowieniaDetails = (req, res, next) => {
                 allpozycjaZamowien: allpozycjaZamowien,
                 pageTitle: 'Szczegóły pozycji zamówień',
                 formAction: '',
-                navLocation: 'pozycjaZamowien'
+                navLocation: 'pozycjaZamowien',
+                validationErrors: []
             });
         });
 }
@@ -105,21 +136,77 @@ exports.showPozycjaZamowieniaDetails = (req, res, next) => {
 // 
 //
 
+
 exports.addPozycjaZamowienia = (req, res, next) => {
+
+    let zamowienieGetAll = ZamowieniaRepository.getZamowienia();
+    let produktGetAll = ProduktRepository.getProdukt();
+    let pozycjaZamowienGetAll = PozycjaZamowienRepository.getZamowienieProdukt();
+
     const pozycjaZamowieniaData = { ...req.body };
     PozycjaZamowienRepository.createZamowienieProdukt(pozycjaZamowieniaData)
         .then(result => {
             res.redirect('/pozycjaZamowien');
+        })
+        .catch(err => {
+            Promise.all([zamowienieGetAll, produktGetAll, pozycjaZamowienGetAll]).then(results => {
+                const [zam, prod, pozZam] = results;
+                res.render('Pages/PozycjaZamówień/FormularzNowejPozycjiZamówień', {
+                    pozycjaZamowien: pozycjaZamowieniaData,
+                    pageTitle: 'Nowa pozycja zamówień',
+                    formMode: 'createNew',
+                    btnLabel: 'Dodaj',
+                    formAction: '/pozycjaZamowien/add',
+                    navLocation: 'pozycjaZamowien',
+                    validationErrors: err.errors,
+                    allZamowienia: zam,
+                    allProdukty: prod,
+                    allpozycjaZamowien: pozZam
+                });
+
+            })
+
+
         });
+
 };
+
+
+
 
 exports.updatePozycjaZamowienia = (req, res, next) => {
     const idPozycjaZamowien = req.body.IdZamowienieProdukt;
     const pozycjaZamowieniaData = { ...req.body };
+
+    let zamowienieGetAll = ZamowieniaRepository.getZamowienia();
+    let produktGetAll = ProduktRepository.getProdukt();
+    let pozycjaZamowienGetAll = PozycjaZamowienRepository.getZamowienieProdukt();
+
     PozycjaZamowienRepository.updateZamowienieProdukt(idPozycjaZamowien, pozycjaZamowieniaData)
         .then(result => {
             res.redirect('/pozycjaZamowien');
+        })
+        .catch(err => {
+            Promise.all([zamowienieGetAll, produktGetAll, pozycjaZamowienGetAll]).then(results => {
+                const [zam, prod, pozZam] = results;
+                res.render('Pages/PozycjaZamówień/FormularzNowejPozycjiZamówień', {
+                    pozycjaZamowien: pozycjaZamowieniaData,
+                    pageTitle: 'Nowa pozycja zamówień',
+                    formMode: 'createNew',
+                    btnLabel: 'Dodaj',
+                    formAction: '/pozycjaZamowien/edit',
+                    navLocation: 'pozycjaZamowien',
+                    validationErrors: err.errors,
+                    allZamowienia: zam,
+                    allProdukty: prod,
+                    allpozycjaZamowien: pozZam
+                });
+
+            })
+
+
         });
+
 };
 
 exports.deletePozycjaZamowienia = (req, res, next) => {
