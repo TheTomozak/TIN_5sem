16a0ea418f0280ef60b71d4e2b73166f9337f05e
TheTomozak <tomaszpalczewski99@gmail.com>
2021-01-29 15:08:40 +0100

    Zablokoawnie dodawania i edycji dla uzytkownika niezalogowanego

--- MP2/tin-projekt-palczewski-s18969/routes/produktyRoute.js
+++ MP2/tin-projekt-palczewski-s18969/routes/produktyRoute.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const authUtils = require('../util/authUtils');
+
 
 const produktyControler = require('../controllers/produktyController');
 
 router.get('/', produktyControler.showProduktiaList);
-router.get('/add', produktyControler.showAddProduktForm);
-router.get('/edit/:idProduktu', produktyControler.showEditProduktForm);
+router.get('/add', authUtils.permitAuthenticatedUser, produktyControler.showAddProduktForm);
+router.get('/edit/:idProduktu', authUtils.permitAuthenticatedUser, produktyControler.showEditProduktForm);
 router.get('/details/:idProduktu', produktyControler.showProduktDetails);
 
-router.post('/add', produktyControler.addProdukt);
-router.post('/edit', produktyControler.updateProdukt);
-router.get('/delete/:idProduktu', produktyControler.deleteProdukt);
+router.post('/add',authUtils.permitAuthenticatedUser, produktyControler.addProdukt);
+router.post('/edit',authUtils.permitAuthenticatedUser, produktyControler.updateProdukt);
+router.get('/delete/:idProduktu',authUtils.permitAuthenticatedUser, produktyControler.deleteProdukt);
 
 module.exports = router;
 
